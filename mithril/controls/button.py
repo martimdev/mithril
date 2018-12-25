927a693491539ec826cd21938b41bed7233bc871
Martim Marques <martimasterdeveloper@gmail.com>
2018-12-25 15:54:03 +0000

    Refactor util shapes

--- mithril/controls/button.py
+++ mithril/controls/button.py
@@ -1,8 +1,5 @@
-import pygame
-
-from mithril.graphics.util_shapes import RoundedRectangle
-from mithril.graphics.shape import Bordered
 from mithril.controls.label import Label
+from mithril.graphics.util_shapes import RoundedRectangle
 
 DEFAULT_COLOR = (229, 229, 229)
 DEFAULT_HOVER_COLOR = (233, 233, 233)
@@ -10,61 +7,35 @@ DEFAULT_BORDER_COLOR = (181, 181, 181)
 DEFAULT_HOVER_BORDER_COLOR = (3, 158, 211)
 
 
-class Button(RoundedRectangle, Bordered):
+class Button(RoundedRectangle):
     def __init__(self, name,
                  relative_x=0,
                  relative_y=0,
                  width=100,
                  height=30,
                  color=DEFAULT_COLOR,
-                 border_color=DEFAULT_BORDER_COLOR,
-                 border_width=1,
                  radius=4
                  ):
         RoundedRectangle.__init__(self, relative_x, relative_y, color, width, height, radius)
-        Bordered.__init__(self, border_color, border_width)
         self.name = name
-        self.border_color = border_color
         self.label = Label(self.name)
         self.add_node(self.label)
+        self.border.color = DEFAULT_BORDER_COLOR
         self.on_mouse_enter_handlers.append(self.mouse_enter_handler)
         self.on_mouse_exit_handlers.append(self.mouse_exit_handler)
 
     def mouse_enter_handler(self):
         self.color = DEFAULT_HOVER_COLOR
-        self.border_color = DEFAULT_HOVER_BORDER_COLOR
+        self.border.color = DEFAULT_HOVER_BORDER_COLOR
 
     def mouse_exit_handler(self):
         self.color = DEFAULT_COLOR
-        self.border_color = DEFAULT_BORDER_COLOR
+        self.border.color = DEFAULT_BORDER_COLOR
 
     def update(self):
         super().update()
-        self.circle1.border_color = self.border_color
-        self.circle2.border_color = self.border_color
-        self.circle3.border_color = self.border_color
-        self.circle4.border_color = self.border_color
         self.label.relative_x = (self.width - self.label.get_width()) // 2
         self.label.relative_y = ((self.height - self.label.get_height()) // 2) - 1
 
-    def draw_border(self, screen):
-        pygame.draw.line(screen, self.border_color,
-                         (self.relative_x + self.radius, self.relative_y),
-                         (self.relative_x - self.radius + self.width, self.relative_y),
-                         self.border_width)
-        pygame.draw.line(screen, self.border_color,
-                         (self.relative_x + self.radius, self.relative_y + self.height),
-                         (self.relative_x - self.radius + self.width, self.relative_y + self.height),
-                         self.border_width)
-        pygame.draw.line(screen, self.border_color,
-                         (self.relative_x, self.relative_y + self.radius),
-                         (self.relative_x, self.relative_y - self.radius + self.height),
-                         self.border_width)
-        pygame.draw.line(screen, self.border_color,
-                         (self.relative_x + self.width, self.relative_y + self.radius),
-                         (self.relative_x + self.width, self.relative_y - self.radius + self.height),
-                         self.border_width)
-
     def draw(self, screen):
         super().draw(screen)
-        self.draw_border(screen)
--- mithril/graphics/util_shapes.py
+++ mithril/graphics/util_shapes.py
@@ -1,10 +1,12 @@
+from mithril.graphics.border import Border
 from mithril.graphics.shape import Shape
-from mithril.graphics.base_shapes import Rectangle, Circle
+from mithril.graphics.base_shapes import Rectangle, Circle, HorizontalLine, VerticalLine
 
 
 class RoundedRectangle(Shape):
     def __init__(self, relative_x, relative_y, color, width, height, radius):
         super().__init__(relative_x, relative_y, color)
+        self.border = Border(relative_x, relative_y, 1, (255, 0, 0))
         self.width = width
         self.height = height
         self.radius = radius
@@ -14,12 +16,17 @@ class RoundedRectangle(Shape):
         self.circle2 = Circle(0, 0, 0, 0, self.radius)
         self.circle3 = Circle(0, 0, 0, 0, self.radius)
         self.circle4 = Circle(0, 0, 0, 0, self.radius)
+        self.line1 = HorizontalLine(0, 0, 0, 0)
+        self.line2 = HorizontalLine(0, 0, 0, 0)
+        self.line3 = VerticalLine(0, 0, 0, 0)
+        self.line4 = VerticalLine(0, 0, 0, 0)
         self.add_node(self.circle1)
         self.add_node(self.circle2)
         self.add_node(self.circle3)
         self.add_node(self.circle4)
         self.add_node(self.rect1)
         self.add_node(self.rect2)
+        self.add_node(self.border)
 
     def is_colliding(self, pos):
         default_nodes = [self.rect1, self.rect2, self.circle1, self.circle2, self.circle3, self.circle4]
@@ -29,6 +36,39 @@ class RoundedRectangle(Shape):
 
     def update(self):
         super().update()
+        self.circle1.border_color = self.border.color
+        self.circle2.border_color = self.border.color
+        self.circle3.border_color = self.border.color
+        self.circle4.border_color = self.border.color
+        self.border.nodes.clear()
+        self.line1 = HorizontalLine(
+            self.relative_x + self.radius,
+            self.relative_y,
+            self.border.color,
+            self.width - self.radius * 2
+        )
+        self.line2 = HorizontalLine(
+            self.relative_x + self.radius,
+            self.relative_y + self.height - 1,
+            self.border.color,
+            self.width - self.radius * 2
+        )
+        self.line3 = VerticalLine(
+            self.relative_x,
+            self.relative_y + self.radius,
+            self.border.color,
+            self.height - self.radius * 2
+        )
+        self.line4 = VerticalLine(
+            self.relative_x + self.width - 1,
+            self.relative_y + self.radius,
+            self.border.color,
+            self.height - self.radius * 2
+        )
+        self.border.add_node(self.line1)
+        self.border.add_node(self.line2)
+        self.border.add_node(self.line3)
+        self.border.add_node(self.line4)
         self.rect1.x = self.relative_x
         self.rect1.y = self.relative_y + self.radius
         self.rect1.width = self.width
@@ -42,19 +82,15 @@ class RoundedRectangle(Shape):
         self.circle1.relative_x = self.relative_x + self.radius
         self.circle1.relative_y = self.relative_y + self.radius
         self.circle1.color = self.color
-        self.circle1.border_color = self.color
         self.circle2.relative_x = self.relative_x - self.radius + self.width - 1
         self.circle2.relative_y = self.relative_y + self.radius
         self.circle2.color = self.color
-        self.circle2.border_color = self.color
         self.circle3.relative_x = self.relative_x + self.radius
         self.circle3.relative_y = self.relative_y - self.radius + self.height - 1
         self.circle3.color = self.color
-        self.circle3.border_color = self.color
         self.circle4.relative_x = self.relative_x - self.radius + self.width - 1
         self.circle4.relative_y = self.relative_y - self.radius + self.height - 1
         self.circle4.color = self.color
-        self.circle4.border_color = self.color
 
     def draw(self, screen):
         for node in self.nodes:
